1477dafaf8454b5f51730b1d4ca93c91f07c83c1
osadx35 <osadx35@gmail.com>
2023-12-29 20:36:14 +0200

    ppp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="mx-auto my-2 flex w-full justify-center text-sm">
+    <div className="mx-auto mt-2 flex w-full justify-center pb-2 text-sm">
       Osama Khalil - © {currentYear} - All rights reserved.
     </div>
   );
