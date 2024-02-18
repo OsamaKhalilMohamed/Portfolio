eb5eaedd11ce7198aa9c3f420983b8e119b3a42e
osadx35 <osadx35@gmail.com>
2024-02-18 20:35:07 +0200

    final final

--- src/pages/Tech/index.tsx
+++ src/pages/Tech/index.tsx
@@ -53,7 +53,7 @@ export default function TechAndTools(): React.ReactElement {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen w-full max-w-[1000px] flex-col items-start justify-start overflow-x-hidden ">
+      <main className="flex min-h-screen w-5/6 max-w-[1000px] flex-col items-start justify-start overflow-x-hidden lg:w-full ">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
