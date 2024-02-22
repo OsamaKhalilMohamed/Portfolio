644bd587ea13aedeb61ec6fcd1dc75b42869a6aa
osadx35 <osadx35@gmail.com>
2024-02-22 23:11:49 +0200

    dsadasda

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider, useTheme } from "next-themes";
 import Footer from "~/components/Footer";
 import Navbar from "~/components/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({
   weight: ["100", "200", "300", "400", "600", "700"],
@@ -16,12 +16,10 @@ const inter = Inter({
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ThemeProvider attribute="class">
-      <main className={`${inter.className} relative min-h-screen`}>
-        <div className="relative flex h-full flex-col items-center justify-center ">
-          <Navbar />
-          <Component {...pageProps} />
-          <Footer />
-        </div>
+      <main className={`${inter.className}`}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
       </main>
     </ThemeProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,12 @@ import Image from "next/image";
 import Projects from "~/components/Projects";
 import synapse2 from "../../public/synapse2.png";
 import { useRef } from "react";
+import { useTheme } from "next-themes";
 
 export default function Home(): React.ReactElement {
   const homeRef = useRef(null);
+  const { theme } = useTheme();
+
   return (
     <>
       <Head>
@@ -19,6 +22,18 @@ export default function Home(): React.ReactElement {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="fixed left-0 top-0 -z-10 h-full w-full">
+        {theme === "dark" ? (
+          <div className="relative h-full w-full bg-black">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+            <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
+          </div>
+        ) : (
+          <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+          </div>
+        )}
+      </div>
       <main className="flex h-full min-h-screen w-full max-w-[100%] flex-col items-center justify-start overflow-hidden scroll-smooth pb-8">
         <div
           id="home"
