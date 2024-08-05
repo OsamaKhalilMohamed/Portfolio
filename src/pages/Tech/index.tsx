68f30e207f5020e30f1066c3fd823faf2704401f
osama khalil <okhalil@synapse-analytics.io>
2024-08-05 18:24:07 +0300

    dddd

--- src/pages/Tech/index.tsx
+++ src/pages/Tech/index.tsx
@@ -53,7 +53,7 @@ export default function TechAndTools(): React.ReactElement {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-full min-h-screen w-full max-w-[100%] flex-col items-center justify-start overflow-hidden scroll-smooth pb-8 dark:bg-zinc-950">
-        <div className="mb-8 w-full max-w-[1000px]">
+        <div className="mb-8 w-5/6 max-w-[1000px]">
           <h1 className="mt-10 px-0 text-start text-5xl font-bold text-[#103090] dark:text-white md:-ml-1.5 md:text-8xl ">
             Tech
           </h1>
@@ -61,12 +61,12 @@ export default function TechAndTools(): React.ReactElement {
             My main tech drivers that i use when building apps.
           </p>
         </div>
-        <ul className="flow-col mb-8 mt-4 h-fit w-full max-w-[1000px] list-disc px-6">
+        <ul className="flow-col mb-8 mt-4 h-fit w-5/6 max-w-[1000px] list-disc px-6">
           {techs?.map((tech) => (
             <div className="mb-8 w-full" key={tech}>
               <li>
                 <span
-                  className={`bg-gradient-to-l from-pink-500 to-yellow-400 bg-clip-text font-inter font-bold text-transparent md:text-4xl`}
+                  className={`bg-gradient-to-l from-pink-500 to-yellow-400 bg-clip-text font-inter text-2xl font-bold text-transparent md:text-4xl`}
                 >
                   {tech}
                 </span>
@@ -76,7 +76,7 @@ export default function TechAndTools(): React.ReactElement {
         </ul>
         <hr className="border-1 w-full border-[#d58322] duration-500  dark:border-[#d2a258]" />
 
-        <div className="mb-8 w-full max-w-[1000px]">
+        <div className="mb-8 w-5/6 max-w-[1000px]">
           <h1 className="mt-10 px-0 text-start text-5xl font-bold text-[#103090] dark:text-white md:-ml-1.5 md:text-8xl ">
             Tools
           </h1>
@@ -84,12 +84,12 @@ export default function TechAndTools(): React.ReactElement {
             My most used software tools for development and personal use.
           </p>
         </div>
-        <ul className="flow-col mb-8 mt-4 h-fit w-full max-w-[1000px] list-disc px-6">
+        <ul className="flow-col mb-8 mt-4 h-fit w-5/6 max-w-[1000px] list-disc px-6">
           {tools?.map((tool) => (
-            <div className="mb-8 w-full" key={tool}>
+            <div className="mb-8 w-5/6" key={tool}>
               <li>
                 <span
-                  className={`bg-gradient-to-r from-indigo-500 to-teal-400 bg-clip-text font-inter font-bold text-transparent md:text-4xl`}
+                  className={`bg-gradient-to-r from-indigo-500 to-teal-400 bg-clip-text font-inter text-2xl font-bold text-transparent md:text-4xl`}
                 >
                   {tool}
                 </span>
