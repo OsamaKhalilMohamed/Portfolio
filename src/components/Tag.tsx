b44cf2797a7ce318e073f55d0c8d864418649b2e
osama khalil <okhalil@synapse-analytics.io>
2024-08-06 16:06:38 +0300

    TAG

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Tag({ title, variant }: Props): React.ReactElement {
   return (
     <div
-      className={`w-[64px] rounded-full bg-[#0c0301] px-2 py-0.5 text-center  text-[#faf1f3] shadow-md dark:bg-[#faf1f3] dark:text-[#0c0301]  md:px-4 md:py-1`}
+      className={`w-[64px] rounded-full bg-[#0c0301] px-2 py-0.5 text-center text-[#faf1f3]  shadow-md dark:bg-[#faf1f3] dark:text-[#0c0301] md:w-[72px]  md:py-1`}
     >
       {title}
     </div>
