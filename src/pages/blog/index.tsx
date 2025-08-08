5568cb1bc90e787ab85b49785b77e1666d1cb9a2
osama khalil <okhalil@synapse-analytics.io>
2025-08-09 01:03:44 +0300

    updated meta in blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { getSortedPostsData } from "../../lib/posts";
 import Link from "next/link";
 
@@ -12,28 +13,49 @@ export async function getStaticProps() {
 
 export default function Blog({ allPostsData }: { allPostsData: any[] }) {
   return (
-    <div className="mx-auto flex h-full min-h-screen w-full  max-w-[100%] justify-center py-10 dark:bg-[#111110]">
-      <div className="w-5/6 max-w-[800px] ">
-        <h1 className="m-auto mb-6  px-8 text-3xl font-bold lg:w-full">
-          All Blogs.
-        </h1>
-        <ul className="m-auto flex  justify-center gap-3 lg:w-full">
-          {allPostsData.map(({ id, date, title, description }) => (
-            <li
-              key={id}
-              className="mb-4 cursor-pointer rounded-xl p-8 transition duration-200 hover:bg-[#F8F8F8] hover:dark:bg-[#1C1C1A]"
-            >
-              <Link href={`/blog/${id}`} className="text-xl text-blue-500 ">
-                {title}
+    <>
+      <Head>
+        <title>Osama Khalil - Blog</title>
+        <meta name="description" content="Osama Khalil - Blog" />
+        <meta property="og:title" content="Osama Khalil" />
+        <meta
+          property="og:description"
+          content="Passionate software developer"
+        />
+        <meta
+          property="og:image"
+          content="https://www.osamakhalil.me/osama2.jpg"
+        />
+        <meta property="og:url" content="https://www.osamakhalil.me/" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="541859888331124" />
 
-                <p className="text-sm text-gray-500">{date}</p>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-                <p className="mt-4 text-sm text-gray-500">{description}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
+      <div className="mx-auto flex h-full min-h-screen w-full  max-w-[100%] justify-center py-10 dark:bg-[#111110]">
+        <div className="w-5/6 max-w-[800px] ">
+          <h1 className="m-auto mb-6  px-8 text-3xl font-bold lg:w-full">
+            All Blogs.
+          </h1>
+          <ul className="m-auto flex  justify-center gap-3 lg:w-full">
+            {allPostsData.map(({ id, date, title, description }) => (
+              <li
+                key={id}
+                className="mb-4 cursor-pointer rounded-xl p-8 transition duration-200 hover:bg-[#F8F8F8] hover:dark:bg-[#1C1C1A]"
+              >
+                <Link href={`/blog/${id}`} className="text-xl text-blue-500 ">
+                  {title}
+
+                  <p className="text-sm text-gray-500">{date}</p>
+
+                  <p className="mt-4 text-sm text-gray-500">{description}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
