69720288666ec14bc10ff6abfaa4859b5234a373
osama khalil <okhalil@synapse-analytics.io>
2025-09-10 16:15:23 +0300

    changed favicon + made the navbar experience sticky with blur effect applied

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import ToggleButton from "./ToggleButton";
 import Link from "next/link";
 
 export default function Navbar(): React.ReactElement {
+  const [scrolled, setScrolled] = useState(false);
+  const triggerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setScrolled(!entry?.isIntersecting),
+      { threshold: 0 }
+    );
+
+    if (triggerRef.current) observer.observe(triggerRef.current);
+
+    return () => {
+      if (triggerRef.current) observer.unobserve(triggerRef.current);
+    };
+  }, []);
+
   return (
     <>
-      <div className=" flex w-full items-center justify-center   dark:bg-[#111110]">
+      <div ref={triggerRef} className="h-px"></div>
+
+      <div
+        className={`sticky top-0 z-50 flex min-h-0 w-full items-center justify-center overflow-hidden pb-4 backdrop-blur-md ${
+          scrolled ? `dark:bg-[#111110]/30` : `dark:bg-[#111110]`
+        }`}
+      >
         <div className="flex w-5/6 max-w-[800px] flex-row-reverse items-center justify-between pt-2">
           <ToggleButton />
           <Link href={"/"}>
